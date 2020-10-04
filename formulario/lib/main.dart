25d305088866494d3810fcbede2e011e776feef4
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-04 09:54:20 -0300

    Corrigi o erro

--- formulario/lib/main.dart
+++ formulario/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Fomrulário'),
+      home: MyHomePage(title: 'Formulário'),
     );
   }
 }
