d3ab4d9b543267f5a903d328fd021bd62dfa248b
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-04 09:50:19 -0300

    Mudei o nome do App

--- formulario/lib/main.dart
+++ formulario/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aplicativo de Formulário',
       theme: ThemeData(
         // This is the theme of your application.
         //
