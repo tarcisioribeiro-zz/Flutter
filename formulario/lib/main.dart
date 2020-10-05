50451230a7d419ade0f37d41fc255efd4104d0a9
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-05 03:35:37 -0300

    testando o ícone

--- formulario/lib/main.dart
+++ formulario/lib/main.dart
@@ -60,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.youtube_searched_for),
       ),
     );
   }
