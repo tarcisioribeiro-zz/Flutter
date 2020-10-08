248525474fc9e1adfecc164769ab9daea878e2ac
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-08 19:54:04 -0300

    Adicionando um botão

--- formulario/lib/main.dart
+++ formulario/lib/main.dart
@@ -16,6 +16,10 @@ class MyApp extends StatelessWidget {
             TextField(),
             TextField(),
             TextField(),
+            RaisedButton(
+              child: Text('Cadastrar'),
+              onPressed: () {},
+            )
           ],
         ),
       ),
