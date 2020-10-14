8e41c9c7d8fc376d98702c1820ace813f084f217
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-14 19:17:27 -0300

    Loop com While

--- Dart-Logic-Programming/loop.dart
+++ Dart-Logic-Programming/loop.dart
@@ -1,6 +1,20 @@
 main() {
   //Loop com for
-  for (int x = 0; x < 10; x = x + 1) {
+
+  // for (int x = 0; x < 10; x = x + 1) {
+  //   print("Rodou $x");
+  // }
+
+  bool condicao = true;
+  int x = 0;
+
+  //Loop com While
+
+  while (condicao) {
     print("Rodou $x");
+    if (x > 9) {
+      condicao = false;
+    }
+    x++;
   }
 }
