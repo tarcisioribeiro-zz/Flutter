eea37a0e808908f00fbe5fc66db6c7cea9ee29c1
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-14 12:46:31 -0300

    Adicionando função com retorno

--- Dart-Logic-Programming/imc.dart
+++ Dart-Logic-Programming/imc.dart
@@ -9,27 +9,29 @@ calculoDeImc() {
 
   String textPeso = stdin.readLineSync();
   int peso = int.parse(textPeso);
-
+  
   print("Digite sua altura: ");
-
   String textAltura = stdin.readLineSync();
   double altura = double.parse(textAltura);
 
-  double calcImc = peso / (altura * altura);
-
-  imprimirResultado(calcImc);
+  double imc = calcImc(peso, altura);
+  imprimirResultado(imc);
 }
 
-imprimirResultado(double calcImc) {
-  if (calcImc < 18.5) {
+double calcImc(int peso, double altura) {
+  return peso / (altura * altura);
+}
+
+imprimirResultado(double imc) {
+  if (imc < 18.5) {
     print("Abaixo do peso.");
-  } else if (calcImc > 18.5 && calcImc < 24.9) {
+  } else if (imc > 18.5 && imc < 24.9) {
     print("Peso ideal.");
-  } else if (calcImc > 25 && calcImc < 29.9) {
+  } else if (imc > 25 && imc < 29.9) {
     print("Acima do peso.");
-  } else if (calcImc > 30 && calcImc < 34.9) {
+  } else if (imc > 30 && imc < 34.9) {
     print("Obesidade grau 1.");
-  } else if (calcImc > 35 && calcImc < 39.9) {
+  } else if (imc > 35 && imc < 39.9) {
     print("Obesidade grau 2.");
   } else {
     print("Obesidade grau 3.");
