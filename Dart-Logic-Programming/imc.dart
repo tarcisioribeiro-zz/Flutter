9a3b83fa7435d7b0ec952ae53a5013c76c7afef2
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-13 23:42:27 -0300

    Adicionando condições (ifs)

--- Dart-Logic-Programming/imc.dart
+++ Dart-Logic-Programming/imc.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 import 'dart:io';
 
 main() {
@@ -20,4 +19,18 @@ main() {
   var calcImc = peso / (altura * altura);
 
   print(calcImc);
+
+  if (calcImc < 18.5) {
+    print("Abaixo do peso.");
+  } else if (calcImc > 18.5 && calcImc < 24.9) {
+    print("Peso ideal.");
+  } else if (calcImc > 25 && calcImc < 29.9) {
+    print("Acima do peso.");
+  } else if (calcImc > 30 && calcImc < 34.9) {
+    print("Obesidade grau 1.");
+  } else if (calcImc > 35 && calcImc < 39.9) {
+    print("Obesidade grau 2.");
+  } else if (calcImc > 40) {
+    print("Obesidade grau 3.");
+  }
 }
