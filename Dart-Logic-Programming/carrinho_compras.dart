96db19c264536ccdce29d0af30084cbbcb3d11aa
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-21 12:59:28 -0300

    Modularizando o projeto

--- Dart-Logic-Programming/carrinho_compras.dart
+++ Dart-Logic-Programming/carrinho_compras.dart
@@ -11,20 +11,26 @@ main() {
       print('=== Terminou o programa ===');
       condicao = false;
     } else if (text == 'Imprimir') {
-      for (var i = 0; i < produtos.length; i++) {
-        print('Item $i - ${produtos[i]}');
-      }
+      imprimir();
     } else if (text == 'Remover') {
-      print('Qual item deseja remover?');
-      for (var i = 0; i < produtos.length; i++) {
-        print('Item $i - ${produtos[i]}');
-      }
-      int item = int.parse(stdin.readLineSync());
-      produtos.removeAt(item);
-      print('Item removido.');
+      remover();
     } else {
       produtos.add(text);
       print('\x1B[2J\x1B[0;0H'); //limpa o terminal
     }
   }
+}
+
+imprimir() {
+  for (var i = 0; i < produtos.length; i++) {
+    print('Item $i - ${produtos[i]}');
+  }
+}
+
+remover() {
+  print('Qual item deseja remover?');
+  imprimir();
+  int item = int.parse(stdin.readLineSync());
+  produtos.removeAt(item);
+  print('Item removido.');
 }
