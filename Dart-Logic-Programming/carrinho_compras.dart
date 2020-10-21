4b95ead56f5724eb5246cde712d32c70ea51890f
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-10-21 12:51:45 -0300

    Removendo um item da Lista

--- Dart-Logic-Programming/carrinho_compras.dart
+++ Dart-Logic-Programming/carrinho_compras.dart
@@ -14,6 +14,14 @@ main() {
       for (var i = 0; i < produtos.length; i++) {
         print('Item $i - ${produtos[i]}');
       }
+    } else if (text == 'Remover') {
+      print('Qual item deseja remover?');
+      for (var i = 0; i < produtos.length; i++) {
+        print('Item $i - ${produtos[i]}');
+      }
+      int item = int.parse(stdin.readLineSync());
+      produtos.removeAt(item);
+      print('Item removido.');
     } else {
       produtos.add(text);
       print('\x1B[2J\x1B[0;0H'); //limpa o terminal
