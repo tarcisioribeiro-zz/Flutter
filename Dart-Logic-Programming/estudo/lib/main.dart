482e3a9961594535050258e2926142e2898e0321
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-10 12:56:44 -0300

    Estrutura de decisão com argumentos.

--- Dart-Logic-Programming/estudo/lib/main.dart
+++ Dart-Logic-Programming/estudo/lib/main.dart
@@ -1,3 +1,7 @@
 void main(List<String> arguments) {
-  print(arguments);
+  if (arguments[0] == 'hackear') {
+    print('Hackeamento concluído.');
+  } else if (arguments[0] == 'imprimir') {
+    print('Executando impressão.');
+  }
 }
