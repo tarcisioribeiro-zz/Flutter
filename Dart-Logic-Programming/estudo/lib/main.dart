16435b54d265afc15aebdc01a1b40182b6d35922
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-10 18:56:04 -0300

    Refatorando cálculo de idade.

--- Dart-Logic-Programming/estudo/lib/main.dart
+++ Dart-Logic-Programming/estudo/lib/main.dart
@@ -1,3 +1,9 @@
+import 'package:estudo/src/calculo_idade.dart';
+
 void main(List<String> arguments) {
-  
+  if (arguments[0] == 'calculo-idade') {
+    calculoIdade();
+  } else {
+    print('Esse programa não existe.');
+  }
 }
---
+++ Dart-Logic-Programming/estudo/lib/src/calculo_idade.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+calculoIdade() {
+  print('Digite sua idade: ');
+  var input = stdin.readLineSync();
+  var idade = int.parse(input);
+
+  if (idade >= 50) {
+    print('Melhor idade.');
+  } else if (idade >= 18) {
+    print('Adulto.');
+  } else if (idade >= 12) {
+    print('Adolescente.');
+  } else {
+    print('Criança.');
+  }
+}
