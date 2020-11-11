821921788b5d4558a9785a8bd3f1103a110797cf
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-11 13:00:29 -0300

    Proteogendo variáveis

--- Dart-Logic-Programming/objetos/lib/main.dart
+++ Dart-Logic-Programming/objetos/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:objetos/src/pessoa.dart';
 
 void main(List<String> arguments) {
   Pessoa pessoa = Pessoa(nome: 'Tarcísio', idade: 22, sexo: 'M');
-  print(pessoa.nome);
-  print(pessoa.idade);
-  print(pessoa.sexo);
+  // print(pessoa.nome);
+  // print(pessoa.idade);
+  // print(pessoa.sexo);
+  pessoa.otherNome = 'Araujo';
+  print(pessoa.otherNome);
 }
--- Dart-Logic-Programming/objetos/lib/src/pessoa.dart
+++ Dart-Logic-Programming/objetos/lib/src/pessoa.dart
@@ -3,5 +3,9 @@ class Pessoa {
   int idade = 22;
   String sexo = 'M';
 
+  final String otherNome = 'D';
+  String _otherNome = 'D';
+  
+
   Pessoa({this.nome, this.idade, this.sexo});
 }
