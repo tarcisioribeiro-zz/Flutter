3d3f44cbad5bbae77c4deb40181a1c7af4fbb653
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-11 12:41:40 -0300

    Método Construtor

--- Dart-Logic-Programming/objetos/lib/main.dart
+++ Dart-Logic-Programming/objetos/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:objetos/src/pessoa.dart';
 
 void main(List<String> arguments) {
-  Pessoa pessoa = Pessoa();
+  Pessoa pessoa = Pessoa('Tarcísio', 22, 'M');
   print(pessoa.nome);
   print(pessoa.idade);
   print(pessoa.sexo);
--- Dart-Logic-Programming/objetos/lib/src/pessoa.dart
+++ Dart-Logic-Programming/objetos/lib/src/pessoa.dart
@@ -2,4 +2,10 @@ class Pessoa {
   String nome = 'Tarcísio';
   int idade = 22;
   String sexo = 'M';
+
+  Pessoa (String nome, int idade, String sexo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+  }
 }
