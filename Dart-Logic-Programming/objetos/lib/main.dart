25c4315cfeec7aba365d154d57e8d3938ecd7292
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2020-11-11 12:34:10 -0300

    Classes.

--- Dart-Logic-Programming/objetos/lib/main.dart
+++ Dart-Logic-Programming/objetos/lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:objetos/src/pessoa.dart';
+
 void main(List<String> arguments) {
-  
+  Pessoa pessoa = Pessoa();
+  print(pessoa.nome);
+  print(pessoa.idade);
+  print(pessoa.sexo);
 }
---
+++ Dart-Logic-Programming/objetos/lib/src/pessoa.dart
@@ -0,0 +1,5 @@
+class Pessoa {
+  String nome = 'Tarcísio';
+  int idade = 22;
+  String sexo = 'M';
+}
