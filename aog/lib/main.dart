3bcbb7e8e35bb3e0cb336c946e745fff9bb3222c
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2021-01-10 23:29:53 -0300

    Criando o Input.

--- aog/lib/main.dart
+++ aog/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'widgets/logo.widget.dart';
 
 void main() {
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +30,39 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Row(
+            children: <Widget>[
+              Container(
+                width: 100,
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "Gasolina",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: TextFormField(
+                  controller: _gasCtrl,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
